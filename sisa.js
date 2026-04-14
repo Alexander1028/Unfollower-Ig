2d6ee97f19f8c7ed73827edbc8ec7eb5f30b7d79
Alexander1028 <tolivosarias12@gmail.com>
2026-04-14 14:09:48 -0500

    Add files via upload
    Este proyecto consiste en una aplicación web diseñada para que los usuarios puedan gestionar y monitorear su comunidad en redes sociales, específicamente enfocándose en el rastreo de seguidores y seguidos (unfollowers).

---
+++ sisa.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousemove', (e) => {
+    const x = (e.clientX / window.innerWidth) * 100;
+    const y = (e.clientY / window.innerHeight) * 100;
+    
+    document.body.style.setProperty('--mouse-x', `${x}%`);
+    document.body.style.setProperty('--mouse-y', `${y}%`);
+});
